b864eff8fe51f176718ae2e496193728544808a5
Sergio Aravena <Sergio Aravena@Sergio>
2015-10-07 19:04:03 -0400

    Subida proyecto

---
+++ src/java/sql/Conexion.java
@@ -0,0 +1,23 @@
+
+package sql;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Conexion {
+    
+        public static Connection getConexion() {
+
+        Connection connection = null;
+        try {
+           String driverClassName = "com.mysql.jdbc.Driver";
+           String driverUrl="jdbc:mysql://localhost/BD";
+           Class.forName(driverClassName);
+           connection = DriverManager.getConnection(driverUrl, "root","");
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+    
+}
